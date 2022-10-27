a6fd1ea5869915f4a3ec12892cd1232eb7723c7a
Tuan.C2207G1 <Pocytamy@gmail.com>
2022-10-27 16:24:30 +0700

    fizzbuzz

---
+++ src/CleancodeRefacting/ex1_1/FizzBuzz.java
@@ -0,0 +1,19 @@
+package CleancodeRefacting.ex1_1;
+
+public class FizzBuzz {
+
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3==0;
+        boolean isBuzz= number % 5==0;
+        if(isBuzz && isFizz)
+            return "FizzBuzz";
+
+        if(isFizz)
+            return "Fizz";
+
+        if(isBuzz)
+            return "Buzz";
+
+        return number + "";
+    }
+}
